f96c8fe9e5d62029c2eed2447d23ce7d7104516f
sanjeevipk <sanjeevipk.ext@calibo.com>
2026-02-02 15:08:50 +0530

    dummy commit monday

--- src/main/java/com/log_to_code/logtocode/controller/PaymentController.java
+++ src/main/java/com/log_to_code/logtocode/controller/PaymentController.java
@@ -19,7 +19,6 @@ public class PaymentController {
     private static final String COMMIT_SHA = System.getenv("COMMIT_SHA") != null ? System.getenv("COMMIT_SHA") : "unknown";
 
 
-
     @GetMapping("/pay/{orderId}")
     public String makePayment(@PathVariable String orderId) {
         try {
