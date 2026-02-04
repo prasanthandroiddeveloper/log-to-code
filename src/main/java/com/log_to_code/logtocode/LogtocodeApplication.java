18d27f7565762956186ddb40ebde7c4a48d7a12d
sanjeevipk <sanjeevipk.ext@calibo.com>
2026-02-04 17:33:55 +0530

    test commit

--- src/main/java/com/log_to_code/logtocode/LogtocodeApplication.java
+++ src/main/java/com/log_to_code/logtocode/LogtocodeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LogtocodeApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(LogtocodeApplication.class, args);
 	}
